39d66e12e002b10f4ed8d18178327945527dbac8
RohanSangle <rohan.sangle02@gmail.com>
2023-06-16 01:34:57 +0530

    basic skeleton homepage

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -4,7 +4,7 @@ import './Heading.css'
 
 function Heading() {
   return (
-    <>
+    
     <div className='container'>
       <nav>
         <h1>Rohan Sangle</h1>
@@ -14,16 +14,9 @@ function Heading() {
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
-
-      <div className='content'>
-        <h2>Hi, I am Rohan Sangle</h2>
-        <p>I am a undergraduate at PCCOE, pursuing B.Tech in Electronics & Telecommunication Engineering. I am also interested in the field of Web Development and have worked on a few projects in this field. I am currently looking for an internship in the field of Development and marketing.</p>
-        <button type="submit">Get In Touch!</button>
-
-      </div>
       
     </div>
-    </>
+    
   )
 }
 
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Homepage.css'
+import bgHome from '../images/bg-home.png'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <>
+    <section>
+      <div className='content'>
+          <h2 className='homename'>Rohan Sangle</h2>
+          <h1>Minimalist Entrepreneur and Developer</h1>
+          <p>I am a undergraduate at PCCOE, pursuing B.Tech in Electronics & Telecommunication Engineering. I am also interested in the field of Web Development and have worked on a few projects in this field. I am currently looking for an internship in the field of Development and marketing.</p>
+      </div>
+
+      <div className='pfpimage'>
+        <img src={bgHome} alt='Rohan Sangle' />
+      </div>
+    </section>
+    </>
   )
 }
 
