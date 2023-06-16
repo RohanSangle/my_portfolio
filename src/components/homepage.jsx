6197306153bd752fdae8521904ed268eb7a0d16c
RohanSangle <rohan.sangle02@gmail.com>
2023-06-17 02:04:51 +0530

    much better homepage🙃

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -10,12 +10,21 @@ function Homepage() {
           <h2 className='homename'>Rohan Sangle</h2>
           <h1>Minimalist Entrepreneur and Developer</h1>
           <p>I am a undergraduate at PCCOE, pursuing B.Tech in Electronics & Telecommunication Engineering. I am also interested in the field of Web Development and have worked on a few projects in this field. I am currently looking for an internship in the field of Development and marketing.</p>
+          <ul>
+            <li><a href="#">More About me</a></li>
+            <li><p>or</p></li>
+            <li><a href="#">View Achievements</a></li>
+          </ul>
       </div>
 
       <div className='pfpimage'>
         <img src={bgHome} alt='Rohan Sangle' />
       </div>
     </section>
+
+    <div className='mousescroll'>
+      <span className='wheel'></span>
+    </div>
     </>
   )
 }
